7a23b30a66ef0f92b3107d5dd5701314f70066f8
XeedOus <100945558+DjeziriOus@users.noreply.github.com>
2025-11-22 19:14:04 +0100

    added saving to file functionality, function called sauvegarderReseau called from FilePersistence into getting called from the AppController, into getting called from the View

--- src/Controller/FilePersistence.java
+++ src/Controller/FilePersistence.java
@@ -2,8 +2,10 @@ package Controller;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 
 import java.util.Map;
@@ -216,6 +218,33 @@ public class FilePersistence {
 
   }
 
+  /**
+   * Saves the current network configuration to a file. [cite: 84, 85]
+   */
   public static void sauvegarderReseau(Reseau reseau, String cheminFichier) throws IOException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(cheminFichier))) {
+
+      // 1. Write Generators
+      for (Generateur gen : reseau.getGenerateurs().values()) {
+        writer.write(String.format("generateur(%s, %d).", gen.getNom(), gen.getCapaciteMax()));
+        writer.newLine();
+      }
+
+      // 2. Write Maisons
+      for (Maison maison : reseau.getMaisons().values()) {
+        writer.write(String.format("maison(%s, %s).", maison.getNom(), maison.getConsommation().name()));
+        writer.newLine();
+      }
+
+      // 3. Write Connexions
+      // We iterate over the map to reconstruct connexions
+      for (Map.Entry<String, List<String>> entry : reseau.getConnexions().entrySet()) {
+        String nomMaison = entry.getKey();
+        for (String nomGen : entry.getValue()) {
+          writer.write(String.format("connexion(%s, %s).", nomGen, nomMaison));
+          writer.newLine();
+        }
+      }
+    }
   }
 }
